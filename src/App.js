6b7a993997c48e6f51a0aa09603434bf48f033c0
AlbanyCanCode-LDR <purelyeducation@outlook.com>
2018-10-21 15:15:32 -0400

    imported css and cleaned up the code and files further

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
 import React, { Component } from 'react';
 
-import './App.css';
+import './retro-board.css';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        
+      //main return div, DO NOT DELETE!!
+
+      <div> 
+
+
+<div className="row">
+  <div className="column" >
+    <h2>Column 1</h2>
+    <p>Some text..</p>
+  </div>
+  <div className="column" >
+    <h2>Column 2</h2>
+    <p>Some text..</p>
+  </div>
+  <div className="column">
+    <h2>Column 3</h2>
+    <p>Some text..</p>
+  </div>
+</div>
+
       </div>
     );
   }
